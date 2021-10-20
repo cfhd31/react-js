6262dc2f15a05bee52e339f06cd9e8984f2fe20e
cfhd31 <cfhd31@hotmail.com>
2021-10-19 22:31:07 -0300

    contador

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 function Card({nave, precio, imagenes}) {
+    var stock = 15
+    const [count, setCount] = useState(1);
+  
+    const aumentarContador = () => {
+      if(count < stock){
+        setCount(count + 1);
+      }
+    };
+  
+    const disminuyeContador = () => {
+      if(count > 1){
+        setCount(count - 1);
+      }
+    };
+
     return (
         <div className="card text-center bg-dark">
             <img src={imagenes} className=" imagenLogo2" />
             <div className="card-body text-light">
                 <h6 className="card-title">Nave: {nave}</h6>
                 <p className="card-text text-secundary">Precio: {precio}</p>
+                
+                <div className="conteiner cantidad justify-content-center d-flex ">
+                    
+                    <button onClick={ disminuyeContador }>-</button>
+                    <h6>{ count }</h6>
+                    <button onClick={ aumentarContador }>+</button>
+                </div>
                 <a href="##"className="btn btn-outline-secondary ">
-                    Comprar
+                    Agregar al carrito
                 </a>
             </div>
         </div>
