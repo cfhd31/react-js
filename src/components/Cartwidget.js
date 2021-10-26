d39c82d084792ac6398049da08b0c0d3cd1c1a68
cfhd31 <cfhd31@hotmail.com>
2021-10-25 22:28:48 -0300

    1

--- src/components/Cartwidget.js
+++ src/components/Cartwidget.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const CartWidget = () => {
   return (
 <div className=""> 
-                        <i className="fas fa-shopping-cart"></i>
-                            <span className="" id="cartCount"> { /*countCards quiero llamar al contadorCards de Card */ } </span>
-                        </div> 
+    <i className="fas fa-shopping-cart"></i>
+        <span className="" id="cartCount"> { /*countCards quiero llamar al contadorCards de Card */ } </span>
+    </div> 
   )
 }
