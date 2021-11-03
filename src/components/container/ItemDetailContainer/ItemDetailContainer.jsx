c2fbd981e4ef9f600b47d0b522ccb61f0bee0b57
cfhd31 <cfhd31@hotmail.com>
2021-11-03 19:23:04 -0300

    detalle de producto x id

--- src/components/container/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/container/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,27 +1,44 @@
 import {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail'
-import { getFetchUnaNave } from '../../../services/GetFetch'
+import { getFetchNaves } from '../../../services/GetFetch'
 
 const ItemDetailContainer = () => {
     
-    const [prod, setProd] = useState({})
+
+    const [productIndiv, setProdIndiv] = useState({})
+    const {id} = useParams()
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        getFetchUnaNave
-        .then( res => {        
-            console.log('llamada a api') 
-            setProd(res)
-        })    
-        .catch(err => console.log(err))
-        .finally(()=> console.log(false))       
-    },[]) 
-      
-    console.log(prod);
+        
+        if(id){
+            getFetchNaves.then((res) => {
+              setProdIndiv(res.find(prod => prod.id === parseInt(id)))
+            })
+            
+              .catch(err => console.log(err))
+              .finally(() => setLoading(false))}
+          else 
+          {
+            getFetchNaves.then((res) => {
+              setProdIndiv(res) 
+            })
+              .catch(err => console.log(err))
+              .finally(() => setLoading(false))
+          }
+        }, [id])
+
+
+    console.log(productIndiv);
 
     return (
+        loading ? <h5>Cargando el producto..</h5>
+            :
         <>
-            <ItemDetail prod={prod} />
+            <ItemDetail prod={productIndiv} />
         </>
+        
     )
 }
 
