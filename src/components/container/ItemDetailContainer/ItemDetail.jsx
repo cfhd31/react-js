1cd306cd1ed109f373a61ca2e900b54030f0da68
cfhd31 <cfhd31@hotmail.com>
2021-11-08 21:18:12 -0300

    8/11/2021

--- src/components/container/ItemDetailContainer/ItemDetail.jsx
+++ src/components/container/ItemDetailContainer/ItemDetail.jsx
@@ -15,47 +15,8 @@ const ItemDetail = ({prod}) => {
         alert("Se agregó " + count + ' ' + prod.nombre)
       }
 
-      const InputCount= ()=> {
-
-        return <button className="btn btn-outline-primary"  onClick={()=>alert('yendo al cart')}>Terminar compra</button>
-    }
-    
-    const ButtonCount= ()=> {
-        return <button className="btn btn-outline-primary" onClick={()=>console.log('Producto agregado')}>Agregar Al carrito</button>
-    
-    }
     
     return (
-        /*<div className="col s12 m12 l12 flex" key={prod.id}>
-            <div className="card ">
-                <div className="card-image waves-effect waves-block waves-light">
-
-
-                    <div className="col s12 m7 l7 detalleProducto bg-dark ">
-                        <h4 className="card-title text-danger font-weight-bold ">{prod.tipo}</h4>
-                        <img src={prod.imagen} alt={prod.tipo}/>
-
-                        <h5 className="producto text-light">{prod.nombre}</h5>
-                        <span className="price text-light">
-                            {(prod.precio)}
-                        </span>
-                        <div className="text-light">
-                            <p>{prod.descripcion}</p>
-                        </div>
-                        <div className="text-light">
-                        <ItemCount stock={prod.stock} initial="1" onAdd={onAdd}/>
-                        </div>
-                        <Link to={`/`} className="text-primary">
-                            <button className="btn btn-outline-primary btn-block">
-                                Volver
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-            </div>
-        </div>*/
-
-
 
         <div className="card text-center bg-dark col-md-6 container-fluid" >
             <h4 className="card-title text-danger font-weight-bold ">{prod.tipo}</h4>
@@ -81,9 +42,10 @@ const ItemDetail = ({prod}) => {
                         inputType === 'input'?
                         <ItemCount stock={prod.stock} initial="1" onAdd={onAdd}/>
                         :
-                        <Link to="/cart" className="pretext comprar">Ir a Carrito</Link>  
+                        <Link to="/cart" className="pretext botonComprar">Ir al Carrito</Link>  
                     }
                 </div>
+                
 
             </div>
         </div>
