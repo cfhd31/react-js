e270a59813fb643aa05538b447ec875e301d8960
cfhd31 <cfhd31@hotmail.com>
2021-11-20 20:20:52 -0300

    20/11

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import {memo} from 'react'
 import Item from '../Item/Item'
 
-function ItemList({product}) {
-    
-    return (           
-        product.map(prod=> <Item key={prod.id} prod={prod} /> )          
-    )
-}
+const ItemList  = memo (
+    ({product}) => {
+        
+        return (           
+            product.map(prod=> <Item key={prod.id} prod={prod} /> )          
+        )
+    }
+)
 
 export default ItemList
--- src/components/container/ItemListContainer/ItemListContainer.jsx
+++ src/components/container/ItemListContainer/ItemListContainer.jsx
@@ -14,12 +14,18 @@ const ItemListContainer = () => {
     const { id } = useParams()
 
     useEffect(() => {
-        const db = getFirestore()
-        const dbQuery = db.collection('items').get()
-        dbQuery
-        .then(resp => setProduct( resp.docs.map( prod => ( { id: prod.id, ...prod.data() } )) ))
-        //.then(resp => setProduct({id: resp.id, ...resp.data})) 
-        //.then(resp => console.log(resp))
+    const db =getFirestore()
+    if (id){
+        const dbQueryCategoria= db.collection('items').where('categoria','==',id).get()
+        dbQueryCategoria.then (resp=> setProducts(resp.docs.map(prod =>({id:prod.id, ...prod.data()}) )))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+    } else {
+        const dbQuery= db.collection('items').get()
+        dbQuery .then (resp=> setProducts(resp.docs.map(prod =>({id:prod.id, ...prod.data()}) )))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+    }
 
     //     if (id) {
     //         getFetchNaves
@@ -40,12 +46,12 @@ const ItemListContainer = () => {
     //     }
      },[id]) 
       
-     console.log (product)
+     console.log (products)
     return (
         loading? <h5>Cargando los productos..</h5> :
         <div className=" container">
-            <div className="acomodar row justify-content-md-center">            
-                <ItemList product={product} />  
+            <div className="acomodar row justify-content-md-center">   
+                <ItemList product={products} />  
             </div>
         </div>
     )
